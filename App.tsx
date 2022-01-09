708b4b646183765aa8599a4141d3159f30a4dd8c
Kesmek <justin.scopelleti@gmail.com>
2022-01-09 18:50:50 -0500

    add big 3 deps: REA, RNGH, safe area context

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
-  return <SafeAreaView style={styles.root} />;
+  return (
+    <GestureHandlerRootView style={styles.flex}>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.root} />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
 };
 
 const styles = StyleSheet.create({
   root: { backgroundColor: 'salmon', height: '100%', width: '100%' },
+  flex: { flex: 1 },
 });
 
 export default App;
